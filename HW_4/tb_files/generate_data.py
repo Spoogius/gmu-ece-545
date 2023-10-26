bdd74629510ceca83b019005cf93e29975672da1
Stewart Schuler <stewart.schuler@nrl.navy.mil>
2023-10-26 11:26:57 -0400

    Fixed issue where hex string would be larger than W/4

--- HW_4/tb_files/generate_data.py
+++ HW_4/tb_files/generate_data.py
@@ -8,14 +8,16 @@ for param in [ {'Case':'i','N':16,'W':8}, {'Case':'ii','N':16,'W':16}, {'Case':'
     data_vals = random.sample( range(0, (2**param['W'])-1 ), param['N'] )
     rand_perm = random.sample(data_vals, len(data_vals) )
 
+    HEX_CHARS = int(param['W']/4)
+    
     # Write Input
     for addr,data in enumerate(rand_perm):
-        fout_input.write(f"{addr:04X} {data:04X}\n")
+        fout_input.write(f"{addr:0{HEX_CHARS}X} {data:0{HEX_CHARS}X}\n")
     
     data_vals.sort(reverse=True)
     # Write Output
     for addr in range(param['N']):
-        fout_output.write(f"{addr:04X} {data_vals[addr]:04X}\n")
+        fout_output.write(f"{addr:0{HEX_CHARS}X} {data_vals[addr]:0{HEX_CHARS}X}\n")
     
     fout_input.close()
     fout_output.close()
