32576f19197c30e7542b425dc717069b580b0b52
Stewart Schuler <stewart.schuler@nrl.navy.mil>
2023-10-26 10:55:18 -0400

    Added testbench input/output data files

---
+++ HW_4/tb_files/generate_data.py
@@ -0,0 +1,22 @@
+import random
+
+for param in [ {'Case':'i','N':16,'W':8}, {'Case':'ii','N':16,'W':16}, {'Case':'iii','N':32,'W':16} ]:
+    fout_input  = open(f"case_{param['Case']}/input.ascii" , 'w')
+    fout_output = open(f"case_{param['Case']}/output.ascii", 'w')
+
+   
+    data_vals = random.sample( range(0, (2**param['W'])-1 ), param['N'] )
+    rand_perm = random.sample(data_vals, len(data_vals) )
+
+    # Write Input
+    for addr,data in enumerate(rand_perm):
+        fout_input.write(f"{addr:04X} {data:04X}\n")
+    
+    data_vals.sort(reverse=True)
+    # Write Output
+    for addr in range(param['N']):
+        fout_output.write(f"{addr:04X} {data_vals[addr]:04X}\n")
+    
+    fout_input.close()
+    fout_output.close()
+        
