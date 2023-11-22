18bfbc69f9107da21f0ed8b8741095d2bb429f1b
Stewart Schuler <stewart.schuler@nrl.navy.mil>
2023-11-22 10:52:57 -0500

    Restructured folder for galois code

--- Project/matlab/galois_testbench.m
+++ Project/matlab/galois_testbench.m
@@ -7,10 +7,10 @@ for a_val = [0:(2^m)-1]
     for b_val = [0:(2^m)-1]
         a = gf( a_val, m );
         b = gf( a_val, m );
-        if mathworks_galois_add( a, b ) ~= galois_add( a, b )
+        if galois.mathworks_galois_add( a, b ) ~= galois.galois_add( a, b )
             fprintf("Galois Add failed in case: a=%d b=%d\n", a.x, b.x );
         end
-        if mathworks_galois_multiply( a, b ) ~= galois_multiply( a, b )
+        if galois.mathworks_galois_multiply( a, b ) ~= galois.galois_multiply( a, b )
             fprintf("Galois Multiply failed in case: a=%d b=%d\n", a.x, b.x );
         end
     end
